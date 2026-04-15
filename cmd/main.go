0612fff59e3906c302ad582c57cc32a14d28247a
KEKACIK <arthur_0p@mail.ru>
2026-04-15 10:07:16 +0300

    Linked struct api and database

--- cmd/main.go
+++ cmd/main.go
@@ -1,13 +1,24 @@
 package main
 
 import (
+	"TestTask/config"
 	"TestTask/internal/api"
+	"TestTask/pkg/logging"
+	"TestTask/pkg/postgresql"
+	"context"
 	"net/http"
 )
 
 func main() {
+	cfg := config.NewConfig()
+	logger := logging.NewLogger(cfg.Debug)
+	client, err := postgresql.NewClient(context.TODO(), 5, cfg.GetPostgresDsn())
+	if err != nil {
+		logger.Fatal(err.Error())
+	}
+
 	mux := http.NewServeMux()
-	handler := api.NewHandler()
+	handler := api.NewHandler(client, logger)
 
 	api.RegisterRoutes(mux, handler)
 
--- internal/api/handler.go
+++ internal/api/handler.go
@@ -1,31 +1,43 @@
 package api
 
-import "net/http"
+import (
+	"TestTask/pkg/logging"
+	"TestTask/pkg/postgresql"
+	"net/http"
+)
 
 type Handler struct {
+	client postgresql.Client
+	logger *logging.Logger
 }
 
 func (h *Handler) Create(w http.ResponseWriter, r *http.Request) {
-
+	// TODO
 }
 
 func (h *Handler) List(w http.ResponseWriter, r *http.Request) {
-
+	// TODO
 }
 
 func (h *Handler) Get(w http.ResponseWriter, r *http.Request) {
-
+	// TODO
 }
 
 func (h *Handler) Update(w http.ResponseWriter, r *http.Request) {
-
+	// TODO
 }
 
 func (h *Handler) Delete(w http.ResponseWriter, r *http.Request) {
-
+	// TODO
 }
 
-func NewHandler() *Handler {
+func NewHandler(
+	client postgresql.Client,
+	logger *logging.Logger,
+) *Handler {
 
-	return &Handler{}
+	return &Handler{
+		client: client,
+		logger: logger,
+	}
 }
