36cd9bcdf54b8df1a77771b0ada38b5b9eade61d
KEKACIK <arthur_0p@mail.ru>
2026-04-15 10:06:08 +0300

    Add logger

---
+++ pkg/logging/logger.go
@@ -0,0 +1,36 @@
+package logging
+
+import (
+	"log/slog"
+	"os"
+	"strings"
+)
+
+type Logger struct {
+	*slog.Logger
+}
+
+func (l *Logger) Fatal(msg string) {
+	l.Error(msg)
+	panic(msg)
+}
+
+func (l *Logger) DebugSQL(q string) {
+	q = strings.ReplaceAll(q, "\t", " ")
+	q = strings.ReplaceAll(q, "\n", "")
+	q = strings.TrimSpace(q)
+
+	l.Debug(q)
+}
+
+func NewLogger(debug bool) *Logger {
+	opts := &slog.HandlerOptions{
+		Level: slog.LevelInfo,
+	}
+	if debug {
+		opts.Level = slog.LevelDebug
+	}
+	return &Logger{
+		slog.New(slog.NewTextHandler(os.Stdout, opts)),
+	}
+}
