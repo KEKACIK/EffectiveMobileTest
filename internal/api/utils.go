9c694c6a09afea8a2e526977377624c209461bec
KEKACIK <arthur_0p@mail.ru>
2026-04-16 12:10:23 +0300

    Add pagination and many fix

--- internal/api/utils.go
+++ internal/api/utils.go
@@ -2,20 +2,44 @@ package api
 
 import (
 	"TestTask/pkg/logging"
+	"TestTask/pkg/utils"
 	"encoding/json"
 	"fmt"
 	"net/http"
 )
 
-func debug(l *logging.Logger, url, method, host string) {
-	l.Debug(fmt.Sprintf("%s %s %s", url, method, host))
+func GetPagination(
+	items []any,
+	total int,
+	page int,
+	limit int,
+) PaginationResponse {
+	// Универсальная функция пагинации
+	// Все структуры необходимо привести в вид Any
+	result := PaginationResponse{Total: total, Page: page, Limit: limit}
+
+	maxPages := utils.RoundUp(total, limit)
+	if page > maxPages {
+		result.Items = []any{}
+		return result
+	}
+
+	startI := (page - 1) * limit
+	endI := utils.Min(total, startI+limit)
+
+	result.Items = items[startI:endI]
+	return result
+}
+
+func debug(l *logging.Logger, url, method, host string, code int) {
+	l.Debug(fmt.Sprintf("%s %s %s - %d", method, url, host, code))
 }
 
 func ErrorResponse(w http.ResponseWriter, code int, err error) {
+	// Универсальная функция возврата ошибки
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(code)
 	json.NewEncoder(w).Encode(map[string]string{
-		"status":  "error",
 		"message": err.Error(),
 	})
 }
