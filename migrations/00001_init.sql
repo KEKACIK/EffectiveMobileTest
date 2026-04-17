7cacf4d6b5fc873f77a1bd1132ea62678532ef4b
KEKACIK <arthur_0p@mail.ru>
2026-04-17 14:56:39 +0300

    Change database structure (add created, updated timestamp columns)

--- migrations/00001_init.sql
+++ migrations/00001_init.sql
@@ -2,10 +2,13 @@
 CREATE TABLE IF NOT EXISTS subscriptions(
     id          SERIAL PRIMARY KEY,
     name        VARCHAR(256) NOT NULL,
-    price       INT,
+    price       INT NOT NULL,
     user_id     UUID NOT NULL,
     start_at    DATE NOT NULL,
-    end_at      DATE NOT NULL
+    end_at      DATE NOT NULL,
+    is_deleted  BOOLEAN DEFAULT false,
+    created_at  TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at  TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
 -- +goose Down
--- migrations/00002_add_is_deleted.sql
+++
@@ -1,5 +0,0 @@
--- +goose Up
-ALTER TABLE subscriptions ADD COLUMN is_deleted BOOLEAN DEFAULT false;
-
--- +goose Down
-ALTER TABLE subscriptions DROP COLUMN is_deleted;
